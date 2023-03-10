791275ce59c868ec26b56b38a8a61b648c249636
Loile <loi33271@gmail.com>
2023-03-10 09:22:06 +0700

    update

--- __manifest__.py
+++ __manifest__.py
@@ -16,26 +16,4 @@
         "views/snippets/v_block.xml",
         "views/snippets/snippets.xml",
     ],
-    "assets": {
-        "tutorial_snippet.assets_tutorial_snippet": [
-            # bootstrap
-            ("include", "web._assets_helpers"),
-            "web/static/src/scss/pre_variables.scss",
-            "web/static/lib/bootstrap/scss/_variables.scss",
-            ("include", "web._assets_bootstrap"),
-            "web/static/src/libs/fontawesome/css/font-awesome.css",  # required for fa icons
-            "web/static/src/legacy/js/promise_extension.js",  # required by boot.js
-            "web/static/src/boot.js",  # odoo module system
-            "web/static/src/env.js",  # required for services
-            "web/static/src/session.js",  # expose __session_info__ containing server information
-            "web/static/lib/owl/owl.js",  # owl library
-            "web/static/lib/owl/odoo_module.js",  # to be able to import "@odoo/owl"
-            "web/static/src/core/utils/functions.js",
-            "web/static/src/core/browser/browser.js",
-            "web/static/src/core/registry.js",
-            "web/static/src/core/assets.js",
-            #
-            "tutorial_theme/static/src/**/*",
-        ],
-    },
 }
